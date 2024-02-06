cbadc5b813bb57599ee00860ef2f0e3f8f2b1b9b
Mayank Ranjan Dayal <mranjandayal@usbosmranjand1.mynetworksettings.com>
2024-02-05 21:47:48 -0500

    ** Apt App **

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent {
+
+  appointment: Appointment = {
+    id: 1,
+    title: 'Appointment',
+    date: new Date()
+  }
+
+}
